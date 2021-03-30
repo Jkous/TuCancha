3fd9d6f9e6cbfc3436cdd329424b7a7959f6fedf
Jorge Muñoz <jorgemartin.dev@gmail.com>
2021-03-29 19:47:35 -0500

    design interfaces

--- app/src/main/java/com/keymobile/tucancha/MainActivity.kt
+++ app/src/main/java/com/keymobile/tucancha/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment.findNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.Navigation
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val boton = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val boton = findViewById<NavigationView>(R.id.bottomNavigationView)
+
+
     }
 }
