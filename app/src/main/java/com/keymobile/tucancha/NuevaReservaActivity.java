9d249aa0b02f858316d7746d989f3128f4477b95
Jorge Muñoz <jorgemartin.dev@gmail.com>
2021-04-25 01:22:41 -0500

    add activity NuevaReserva

---
+++ app/src/main/java/com/keymobile/tucancha/NuevaReservaActivity.java
@@ -0,0 +1,24 @@
+package com.keymobile.tucancha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaReservaActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_reserva);
+
+        AsignarReferencias();
+    }
+
+    private void AsignarReferencias() {
+
+    }
+
+
+
+}
